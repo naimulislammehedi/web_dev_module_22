6c10a776f20396b77f28a6d25caeb896b3fdf433
Mehedi <mehedi.interface@gmail.com>
2026-02-14 10:02:22 +0600

    22-2 Traversing dom - getElementsByTagName

--- js/hello.js
+++ js/hello.js
@@ -1,1 +1,12 @@
-console.log("Hello from JS"); 
+const liCollection = document.getElementsByTagName('li'); 
+console.log(liCollection); 
+
+for (const li of liCollection) {
+    console.log(li.innerText);
+}
+
+const allHeading = document.getElementsByTagName('h1'); 
+
+for (const h1 of allHeading) {
+    console.log(h1.innerText); 
+}
